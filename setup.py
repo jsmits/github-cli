6c869c0fa4ecb06c71ff368c9382d40296981c8e
Sander Smits <jhmsmits@gmail.com>
2009-04-28 14:03:37 +0200

    description update

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     version = version,
     url = 'http://github.com/jsmits/github-cli',
     license = 'BSD',
-    description = "A command-line interface to Github's API.",
-    long_description = """for now, `github-cli <http://github.com/jsmits/github-cli/>`_ 
+    description = "A command-line interface to the GitHub Issues API v2.",
+    long_description = """`github-cli <http://github.com/jsmits/github-cli/>`_ 
 provides a script called ``gh-i``, that can be used to access all of `GitHub 
 <http://www.github.com/>`_'s documented `Issues API <http://develop.github.com/p/issues.html>`_ 
 (v2) functionality from your command-line""",
