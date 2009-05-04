a249dfd442d5a5d91f8302b11bc496ab7f09ed52
Sander Smits <jhmsmits@gmail.com>
2009-05-04 20:34:32 +0200

    improved testing by using  `nose`

--- setup.py
+++ setup.py
@@ -1,40 +1,8 @@
 import os
 from setuptools import setup, find_packages
-from distutils.core import Command
-from unittest import TextTestRunner, TestLoader
-from glob import glob
-from os.path import splitext, basename, join as pjoin, walk
 
-version = '0.2.3'
+version = '0.2.4'
 
-class TestRunner(Command):
-    """run tests with setup.py
-    
-    usage: python setup.py test
-    """
-    user_options = []
-
-    def initialize_options(self):
-        self._dir = os.getcwd()
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        '''
-        Finds all the tests modules in tests/, and runs them.
-        '''
-        testfiles = [ ]
-        for t in glob(pjoin(self._dir, 'tests', '*.py')):
-            if not t.endswith('__init__.py'):
-                testfiles.append('.'.join(
-                    ['tests', splitext(basename(t))[0]])
-                )
-
-        tests = TestLoader().loadTestsFromNames(testfiles)
-        t = TextTestRunner(verbosity = 1)
-        t.run(tests)
-        
 cur_dir = os.path.dirname(__file__)
 readme = open(os.path.join(cur_dir, 'README.rst')).read()
 long_description = readme
@@ -66,6 +34,6 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development :: Bug Tracking',
     ],
-    cmdclass = {'test': TestRunner},
+    test_suite = 'nose.collector',
 )
 
---
+++ tests/test_issues_cli.py
@@ -0,0 +1,57 @@
+import os
+import sys
+from nose.tools import assert_raises
+
+from github.issues import main
+
+repo = 'jsmits/github-cli-public-test'
+prog = 'ghi'
+
+def test_commands():
+    for cmd, exp in test_input:
+        def check_command(cmd, exp):
+            base = [prog, '-r', repo]
+            args = cmd.split(' ')
+            if not args == ['']: # need this for 'just `ghi`' command test
+                base.extend(args)
+            sys.argv = base
+            if type(exp) == type(Exception):
+                assert_raises(exp, main)
+            else:
+                assert main() == exp
+        check_command.description = "command: %s %s" % (prog, cmd)
+        yield check_command, cmd, exp
+
+test_input = (
+    # list commands
+    ('list', None), ('list -v', None), ('', None), ('-v', None),
+    ('lis', "error: command 'lis' not implemented"),
+    ('l', "error: command 'l' not implemented"),
+    ('list -s open', None), ('list -s o', None), ('list -s closed', None),
+    ('list -s c', None), ('list -s all', None), ('list -s a', None),
+    ('-s a', None), ('-s a -v', None), ('list -s close', SystemExit), 
+    
+    # show commands
+    ('show 1', None), ('1', None), ('17288182', "error: issue not found"),
+    
+    # state modification commands
+    ('close 1', None), ('open 1', None), ('c 1', None), ('close 1', None), 
+    ('o 1', None), ('open 1', None),
+    
+    # label commands
+    ('label add testing 1', None), ('label remove testing 1', None),
+    ('al testing 1', None), ('rl testing 1', None),
+    ('label add testing', "error: number required\nexample: ghi label add "\
+        "testing 1"),
+    
+    # help commands
+    ('--help', SystemExit), ('-h', SystemExit),
+    
+    # browser commands
+    ('-w', SystemExit), ('1 -w', SystemExit),
+    
+    # search commands
+    ('search test', None), ('s test', None), ('search test -s open', None),
+    ('search test -s o', None), ('search test -s closed', None),
+    ('search test -s c', None), ('s test -s c', None), ('search', SystemExit),
+)
