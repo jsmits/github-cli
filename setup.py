06d01902e1491bbf6659532dd066c6cd40c4983f
Sander Smits <jhmsmits@gmail.com>
2010-03-28 22:27:40 +0200

    bumped version number; ghi now also works with hg-git controlled repos

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.2.5.2'
+version = '0.2.6'
 
 description = "A command-line interface to the GitHub Issues API v2."
 cur_dir = os.path.dirname(__file__)
